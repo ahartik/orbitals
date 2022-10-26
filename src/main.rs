38c91c5aeee658ee5d53c2bf772c7dd3e3232afa
Aleksi Hartikainen <ahartik@gmail.com>
2022-10-26 22:02:13 +0300

    Hydrogen rendering, mouse support etc

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ use std::borrow::Cow;
 
 use bytemuck::{Pod, Zeroable};
 
+use wgpu::util::DeviceExt;
 use winit::{
-    event::{Event, WindowEvent},
+    event::{Event, WindowEvent, DeviceEvent},
     event_loop::{ControlFlow, EventLoop},
     window::Window,
 };
-use wgpu::util::DeviceExt;
+
+extern crate nalgebra as na;
 
 #[repr(C)]
 #[derive(Copy, Clone, Debug, Pod, Zeroable)]
@@ -15,30 +17,191 @@ struct Vertex {
     position: [f32; 2],
 }
 
+const fn vertex(x: f32, y: f32) -> Vertex {
+    return Vertex {
+        position: [x as f32, y as f32],
+    };
+}
+
 impl Vertex {
     fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
         wgpu::VertexBufferLayout {
             array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
             step_mode: wgpu::VertexStepMode::Vertex,
-            attributes: &[
-                wgpu::VertexAttribute {
-                    offset: 0,
-                    shader_location: 0,
-                    format: wgpu::VertexFormat::Float32x2,
-                }
-            ]
+            attributes: &[wgpu::VertexAttribute {
+                offset: 0,
+                shader_location: 0,
+                format: wgpu::VertexFormat::Float32x2,
+            }],
         }
     }
 }
 
 const QUAD_VERTICES: [Vertex; 6] = [
-    -1.0, -1.0,
-    1.0, -1.0,
-    1.0, 1.0,
-    1.0, 1.0,
-    -1.0, 1.0,
-    -1.0, -1.0
+    vertex(-1.0, -1.0),
+    vertex(1.0, -1.0),
+    vertex(1.0, 1.0),
+    vertex(1.0, 1.0),
+    vertex(-1.0, 1.0),
+    vertex(-1.0, -1.0),
 ];
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, Pod, Zeroable)]
+struct Uniforms {
+    // Extra size for alignment
+    camera_pos: [f32; 4],
+    // Same her
+    look_matrix: [[f32; 4]; 3],
+}
+
+type Vec3f = na::Vector3<f32>;
+type Vec3d = na::Vector3<f64>;
+// type Mat3f = na::Matrix3<f32>;
+
+impl Uniforms {
+    // fn new(pos: &Vec3f, look_at: &Vec3f, up: 
+    fn new(pos: Vec3f, look_at: Vec3f, aspect_ratio: f32) -> Self {
+        // We're converting from "screen" coordinates into our physics coordinates.
+        // In screen coordinates X and Y go from -1 to 1 from bottom left to bottom right.
+        // Z is constant 1.
+        // 
+        // In "physics coordinates" Z is up/down, X is horizontal and Y is depth.
+        
+        // This is multiplied by screen-Z (i.e. 1)
+        let look_z : Vec3f = (look_at - pos).normalize();
+        // This is "up"
+        let look_y = Vec3f::new(0.0,0.0,1.0);
+
+        let mut look_x : Vec3f = look_z.cross(&look_y).normalize();
+        let mut look_y : Vec3f = look_x.cross(&look_z).normalize();
+
+        // Correct for FOV and aspect ratio
+        let fov = (75.0 / 180.0)*std::f32::consts::PI;
+        look_x *= (fov/2.0).sin();
+        look_y *= (fov/2.0).sin()/aspect_ratio;
+
+        // println!("look_x: {}", look_x);
+        // println!("look_y: {}", look_y);
+        // println!("look_z: {}", look_z);
+
+        let mut look_mat_array : [[f32; 4]; 3] = [[0.0; 4]; 3];
+
+        for i in 0..3 {
+            look_mat_array[0][i] = look_x[i];
+            look_mat_array[1][i] = look_y[i];
+            look_mat_array[2][i] = look_z[i];
+        }
+        return Uniforms {
+            camera_pos: *pos.push(0.0).as_ref(),
+            look_matrix: look_mat_array
+        }
+    }
+}
+
+struct UniformBuffer {
+    // uniforms: Uniforms,
+}
+
+impl UniformBuffer {
+    fn layout<'a>() -> wgpu::BindGroupLayoutDescriptor<'a> {
+        wgpu::BindGroupLayoutDescriptor {
+            entries: &[
+                wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                },
+            ],
+            label: Some("uniform_bind_group"),
+        }
+    }
+
+    /*
+     fn desc<'a>(layout: &'a wgpu::BindGroupLayout,
+         buffer: &'a wgpu::Buffer)-> wgpu::BindGroupDescriptor<'a> {
+         return
+             wgpu::BindGroupDescriptor {
+                 label: Some("my uniforms"),
+                 layout,
+                 entries: &[
+             wgpu::BindGroupEntry {
+             binding: 0,
+             resource:
+                 wgpu::BindingResource::Buffer(
+                     wgpu::BufferBinding {
+                         buffer,
+                         offset: 0,
+                         size: wgpu::BufferSize::new(std::mem::size_of::<[f32; 3]>() as u64),
+                     })
+         }]
+             };
+     }
+    */
+}
+
+struct CameraController {
+    is_mouse_pressed: bool,
+    r: f64,
+    theta: f64,
+    phi: f64,
+}
+
+
+impl CameraController {
+    const SENS_X: f64 = 0.004;
+    const SENS_Y: f64 = 0.004;
+    const PI : f64 = std::f64::consts::PI;
+
+    fn new() -> Self {
+        Self{
+            is_mouse_pressed: false,
+            r: 45.0,
+            theta: Self::PI/2.0,
+            phi: (3.0 * Self::PI/2.0),
+        }
+
+    }
+
+    fn process_mouse(&mut self, event: &DeviceEvent) -> bool {
+        match event {
+            DeviceEvent::MouseMotion {
+                delta: (dx, dy)
+            } => {
+                if self.is_mouse_pressed {
+                    println!("move {}, {}", dx, dy);
+                    self.phi += Self::SENS_X * dx;
+                    self.phi %= 2.0 * Self::PI;
+                    self.theta -= Self::SENS_Y * dy;
+                    self.theta = self.theta.clamp(0.0, Self::PI);
+                
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            DeviceEvent::Button { button, state } => {
+                println!("button: {}", button);
+                self.is_mouse_pressed = *state == winit::event::ElementState::Pressed;
+                return false;
+
+            }
+            _ => false
+        }
+    }
+
+    fn camera_pos(&self) -> Vec3f {
+        return (self.r as f32) * Vec3d::new(
+            self.theta.sin() * self.phi.cos(),
+            self.theta.sin() * self.phi.sin(),
+            self.theta.cos()).cast::<f32>();
+    }
+}
 
 async fn run(event_loop: EventLoop<()>, window: Window) {
     let size = window.inner_size();
@@ -46,7 +209,7 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
     let surface = unsafe { instance.create_surface(&window) };
     let adapter = instance
         .request_adapter(&wgpu::RequestAdapterOptions {
-            power_preference: wgpu::PowerPreference::default(),
+            power_preference: wgpu::PowerPreference::HighPerformance,
             force_fallback_adapter: false,
             // Request an adapter which can render to our surface
             compatible_surface: Some(&surface),
@@ -58,11 +221,16 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
     let (device, queue) = adapter
         .request_device(
             &wgpu::DeviceDescriptor {
-                label: None,
+                label: Some("the_device"),
                 features: wgpu::Features::empty(),
-                // Make sure we use the texture resolution limits from the adapter, so we can support images the size of the swapchain.
-                limits: wgpu::Limits::downlevel_webgl2_defaults()
-                    .using_resolution(adapter.limits()),
+
+                // WebGL doesn't support all of wgpu's features, so if
+                // we're building for the web we'll have to disable some.
+                limits: if cfg!(target_arch = "wasm32") {
+                    wgpu::Limits::downlevel_webgl2_defaults()
+                } else {
+                    wgpu::Limits::default()
+                },
             },
             None,
         )
@@ -71,38 +239,72 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
 
     // Load the shaders from disk
     let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
-        label: None,
+        label: Some("Shader"),
         source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("shader.wgsl"))),
     });
 
-    let vertex_buf = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+    let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
         label: Some("Vertex Buffer"),
         contents: bytemuck::cast_slice(&QUAD_VERTICES),
         usage: wgpu::BufferUsages::VERTEX,
     });
 
+    // UNIFORM STUFF
+    let zero_uniforms = Uniforms::zeroed();
+
+    let uniform_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("Uniform Buffer"),
+        contents: bytemuck::cast_slice(&[zero_uniforms]),
+        usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+    });
+
+    let uniform_layout = device.create_bind_group_layout(&UniformBuffer::layout());
+
+    let uniform_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        label: Some("my uniforms"),
+        layout: &uniform_layout,
+        entries: &[
+            wgpu::BindGroupEntry {
+            binding: 0,
+            resource: uniform_buffer.as_entire_binding(),
+                }],
+    });
+
+    // PIPELINE STUFF
     let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-        label: None,
-        bind_group_layouts: &[],
+        label: Some("MyPipelineLayout"),
+        bind_group_layouts: &[&uniform_layout],
         push_constant_ranges: &[],
     });
 
     let swapchain_format = surface.get_supported_formats(&adapter)[0];
-
     let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-        label: None,
+        label: Some("Render pipeline"),
         layout: Some(&pipeline_layout),
         vertex: wgpu::VertexState {
             module: &shader,
             entry_point: "vs_main",
-            buffers: &[],
+            buffers: &[Vertex::desc()],
         },
         fragment: Some(wgpu::FragmentState {
             module: &shader,
             entry_point: "fs_main",
             targets: &[Some(swapchain_format.into())],
         }),
-        primitive: wgpu::PrimitiveState::default(),
+        // primitive: wgpu::PrimitiveState::default(),
+        primitive: wgpu::PrimitiveState {
+            topology: wgpu::PrimitiveTopology::TriangleList,
+            strip_index_format: None,
+            front_face: wgpu::FrontFace::Ccw,
+            cull_mode: Some(wgpu::Face::Back),
+            // Setting this to anything other than Fill requires Features::POLYGON_MODE_LINE
+            // or Features::POLYGON_MODE_POINT
+            polygon_mode: wgpu::PolygonMode::Fill,
+            // Requires Features::DEPTH_CLIP_CONTROL
+            unclipped_depth: false,
+            // Requires Features::CONSERVATIVE_RASTERIZATION
+            conservative: false,
+        },
         depth_stencil: None,
         multisample: wgpu::MultisampleState::default(),
         multiview: None,
@@ -117,6 +319,8 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
         alpha_mode: surface.get_supported_alpha_modes(&adapter)[0],
     };
 
+    let mut camera = CameraController::new();
+    let mut aspect_ratio = (size.width as f32) / (size.height as f32);
     surface.configure(&device, &config);
 
     event_loop.run(move |event, _, control_flow| {
@@ -134,10 +338,17 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
                 // Reconfigure the surface with the new size
                 config.width = size.width;
                 config.height = size.height;
+
+                aspect_ratio = (size.width as f32) / (size.height as f32);
                 surface.configure(&device, &config);
                 // On macos the window needs to be redrawn manually after resizing
                 window.request_redraw();
-            }
+            },
+            Event::DeviceEvent{ event, device_id:_} => {
+                if camera.process_mouse(&event) {
+                    window.request_redraw();
+                }
+            },
             Event::RedrawRequested(_) => {
                 let frame = surface
                     .get_current_texture()
@@ -145,6 +356,8 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
                 let view = frame
                     .texture
                     .create_view(&wgpu::TextureViewDescriptor::default());
+                let uniforms = Uniforms::new(camera.camera_pos(), Vec3f::zeros(), aspect_ratio);
+                queue.write_buffer(&uniform_buffer, 0, bytemuck::cast_slice(&[uniforms]));
                 let mut encoder =
                     device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
                 {
@@ -161,12 +374,15 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
                         depth_stencil_attachment: None,
                     });
                     rpass.set_pipeline(&render_pipeline);
-                    rpass.draw(0..3, 0..1);
+                    rpass.set_vertex_buffer(0, vertex_buffer.slice(..));
+                    rpass.set_bind_group(0, &uniform_bind_group, &[]);
+                    rpass.draw(0..(QUAD_VERTICES.len() as u32), 0..1);
                 }
+
 
                 queue.submit(Some(encoder.finish()));
                 frame.present();
-            },
+            }
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
                 ..
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -1,38 +1,121 @@
 
-struct VertexOutput {
-  @location(0) tex_coord: vec2<f32>,
-  @builtin(position) position: vec4<f32>,
+struct VertexInput {
+  @location(0) position: vec2<f32>,
 };
+struct VertexOutput {
+  @builtin(position) position: vec4<f32>,
+  // This doesn't get transformed
+  @location(0) pos: vec2<f32>,
+};
+
+
+@vertex
+fn vs_main(input: VertexInput) -> VertexOutput {
+    var pos = vec4<f32>(1.0*input.position, 0.0, 1.0);
+    var result: VertexOutput;
+    result.position = pos;
+    result.pos = input.position;
+    return result;
+}
+
+struct Uniforms {
+  camera_pos: vec3<f32>,
+  look_matrix: mat3x3<f32>,
+}
 
 @group(0)
 @binding(0)
-var<uniform> transform: mat4x4<f32>;
+var<uniform> uniforms: Uniforms;
+// @group(0)
+// @binding(1)
+// var<uniform> look_at: vec3<f32>;
 
-// @vertex
-// fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> @builtin(position) vec4<f32> {
-//     let x = f32(i32(in_vertex_index) - 1);
-//     let y = f32(i32(in_vertex_index & 1u) * 2 - 1);
-//     return vec4<f32>(x, y, 0.0, 1.0);
-// }
-// @fragment
-// fn fs_main() -> @location(0) vec4<f32> {
-//     return vec4<f32>(1.0, 0.0, 0.0, 1.0);
-// }
+let PI : f32 = 3.14159265;
 
-@vertex
-fn vs_main(
-    @location(0) position: vec4<f32>,
-    @location(1) tex_coord: vec2<f32>,
-) -> VertexOutput {
-    var result: VertexOutput;
-    result.tex_coord = tex_coord;
-    result.position = transform * position;
-    return result;
+fn hydrogen(pos: vec3<f32>) -> f32 {
+  var r = length(pos);
+  // let theta = atan2(r, pos.z);
+  let costheta = pos.z / r;
+  let sintheta = length(vec2(pos.x,pos.y)) / r;
+  // let sintheta = sin(theta);
+
+  // Y00:
+  // var yfun = sqrt(1.0/(4.0*PI));
+  // Y10:
+  var yfun = sqrt(3.0/(4.0*PI)) * costheta;
+  // Y11:
+  // var yfun = sqrt(3.0/(8.0*PI)) * sintheta;
+
+  // R10
+  // var rfun = 2.0 * exp(-r);
+  // R21
+  var rfun = (1.0/(2.0*sqrt(6.0))) *r* exp(-0.5 * r);
+  // R31
+  var rfun = (8.0/(27.0*sqrt(6.0)))*(1.0-r/6.0)*r* exp(-r/3.0);
+
+
+  var wave = yfun * rfun;
+  return wave * wave;
+}
+
+fn normal(pos: vec3<f32>, cval: f32) -> vec3<f32> {
+  let H  = 0.05;
+  let ddx = (hydrogen(pos + vec3(H, 0.0, 0.0))-cval) / H;
+  let ddy = (hydrogen(pos + vec3(0.0, H, 0.0))-cval) / H;
+  let ddz = (hydrogen(pos + vec3(0.0, 0.0, H))-cval) / H;
+  return -normalize(vec3(ddx, ddy, ddz));
+}
+
+fn lighting(pos: vec3<f32>, normal: vec3<f32>, light_pos: vec3<f32>) -> vec4<f32>{
+  let AMBIENT_COLOR = vec3(1.0, 1.0, 1.0);
+
+  var color = vec3(0.0, 0.0, 0.0);
+  color += 0.3 * AMBIENT_COLOR;
+
+  let to_light = normalize(light_pos - pos);
+  // Diffuse lighting
+  color += max(0.0, 0.7 * dot(normal, to_light)) * vec3(1.0, 1.0, 1.0);
+
+  return vec4(color, 1.0);
 }
 
 @fragment
 fn fs_main(vertex: VertexOutput) -> @location(0) vec4<f32> {
-    let tex = textureLoad(r_color, vec2<i32>(vertex.tex_coord * 256.0), 0);
-    let v = f32(tex.x) / 255.0;
-    return vec4<f32>(1.0 - (v * 5.0), 1.0 - (v * 15.0), 1.0 - (v * 50.0), 1.0);
+
+  var pos = uniforms.camera_pos;
+  var light_pos = vec3(normalize(vec2(pos.x, pos.y)) * 100.0 , 100.0);
+  var v = vec3(vertex.pos.x,   vertex.pos.y, 1.0);
+  var ray = normalize(uniforms.look_matrix * v);
+  // var ray = v;
+
+
+  // let LIMIT = 0.0015;
+  // let LIMIT = 0.0015;
+  
+  // Matches 0.25/nm^3 from Griffiths page 153
+  let LIMIT = 3.7e-05;
+
+  let N = 512;
+  let MAX_DIST = 2.0 * length(pos);
+  let DX = MAX_DIST / f32(N);
+  let dpos = DX * ray;
+  var dist = 0.0;
+  var m = -1.0;
+  for (var i: i32 = 0; i < N;  i++) {
+    let h = hydrogen(pos);
+    if h > LIMIT {
+      // dist /= MAX_DIST;
+      // return vec4(dist, dist, dist, 1.0);
+       let n = normal(pos, h);
+       // return vec4(vec3(0.5, 0.5, 0.5) + 0.5*n, 1.0);
+       // return vec4(n, 1.0);
+       return lighting(pos, n, light_pos);
+    }
+    m = max(m, h);
+    pos += dpos;
+    dist += DX;
+  }
+  // return vec4(vec3(0.5, 0.5, 0.5) - 0.5*ray, 1.0);
+  // return vec4(m, m, m, 1.0);
+  return vec4(0.5, 0.2, 0.7, 1.0);
 }
